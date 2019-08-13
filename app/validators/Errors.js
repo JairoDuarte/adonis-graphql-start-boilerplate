291cf6a11943dbec677eb962f1eda6b3ba29f63f
Jairo Duarte <alfredojairo17@hotmail.com>
2019-08-13 03:43:43 +0100

    Update graphql errors

--- app/validators/Errors.js
+++ app/validators/Errors.js
@@ -1,26 +1,35 @@
 const { ApolloError } = require('apollo-server');
 
-function notFound(message = 'Ressource not found') {
-  return new ApolloError(message, 404);
-}
+const Logger = use('Logger');
 
-function unauthenticated(message = 'Unauthenticated user') {
-  return new ApolloError(message, 401);
-}
+const notFound = (message = 'Ressource not found') => new ApolloError(message, 404);
 
-function badRequest(message = 'Bad Request') {
-  return new ApolloError(message, 400);
-}
+const unauthenticated = (message = 'Missing or invalid jwt token') => new ApolloError(message, 401);
 
-function unprocessableEntity(message = 'Unprocessable entity') {
-  return new ApolloError(message, 422);
-}
+const badRequest = (message = 'Bad Request') => new ApolloError(message, 400);
 
-function internalServer(message = 'Internal Server Error') {
-  return new ApolloError(message, 500);
+const unprocessableEntity = (message = 'Unprocessable entity') => new ApolloError(message, 422);
+
+const internalServer = (message = 'Internal Server Error') => new ApolloError(message, 500);
+
+function addMessageError(error, message) {
+  if (message.includes('PasswordMisMatchException')) {
+    Logger.error(message);
+    return error('invalid password');
+  }
+  if (message.includes('Topology was destroyed') || message.includes('failed to connect to server')) {
+    Logger.error(message);
+    return internalServer();
+  }
+  if (message.includes('validation')) {
+    return error(message);
+  }
+
+  return error();
 }
 
 module.exports = {
+  addMessageError,
   notFound,
   unauthenticated,
   badRequest,
